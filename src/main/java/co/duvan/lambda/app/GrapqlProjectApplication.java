c178425b51b3422bff49266903c25f752476cdff
DUVAN0829 <duvthan067@gmail.com>
2026-01-09 14:09:17 -0500

    Commit inicial

---
+++ src/main/java/co/duvan/lambda/app/GrapqlProjectApplication.java
@@ -0,0 +1,13 @@
+package co.duvan.lambda.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapqlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapqlProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/duvan/lambda/app/GrapqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.lambda.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapqlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
